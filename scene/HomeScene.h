a6697f5bf4076c33dc054fe4246dfbd019089a5a
k020g1146 <k020g1146@g.neec.ac.jp>
2023-02-02 11:25:10 +0900

    HomeSceneの基底クラス変更

--- scene/HomeScene.h
+++ scene/HomeScene.h
@@ -1,126 +1,57 @@
 #pragma once
-#include "BaseScene.h"
+#include "BaseGameScene.h"
 
-#include "../Game/3D/SampleFbxObject/SampleFbxObject.h"
-#include "../Game/3D/SampleObjObject/SampleObjObject.h"
-#include "../Engine/3D/TouchableObject.h"
-
-#include "../Game/3D/Player/Player.h"
-#include "../Game/3D/Plane/Planes.h"
-#include "../Game/3D/AreaOut/AreaOutRock.h"
 #include "../Game/3D/Coins/Coins.h"
-#include "../Game/3D/Exit/Exit.h"
-
-#include "../Game/Collision/CollisionSystem/CollisionPrimitive.h"
-#include "../Game/Collision/CollisionSystem/CollisionManager.h"
-
-#include "../Game/System/RhythmManager.h"
-#include "../Game/System/GameManager.h"
-
-#include <list>
-#include <time.h>
-
 #include "../Game/3D/Enemy/TrainingDummy.h"
 
-class HomeScene : public BaseScene
+class HomeScene : public BaseGameScene
 {
-private:
-	//シーン遷移
-	const float FadeSecond = 1.f;
-
 public:
-	
-	//コンストラクタ
 	HomeScene(DirectXCommon* dxCommon, Window* window, int saveHP = 5);
 
-	/// <summary>
-	/// 起動時
-	/// </summary>
-	void Application() override;
-
-	/// <summary>
-	/// 初期化
-	/// </summary>
-	void Initialize() override;
-
-	/// <summary>
-	/// 更新
-	/// </summary>
-	void Update() override;
-
-	/// <summary>
-	/// 描画
-	/// </summary>
-	void Draw() override;
-
-	/// <summary>
-	/// 後処理
-	/// </summary>
-	void Finalize() override;
-
 private:
 	//シーン遷移
-	void NextSceneChange();
-	void SceneGameEnd();
-	void SceneChange();
+	void NextSceneChange() override;
+
+	///追加
+
+	//初期化
+	void AddCommonInitialize()  override;
+	void AddObject3DInitialize()  override;
+	void AddObject2DInitialize() override;
+
+	//更新
+	void AddInputUpdate() override;
+	void AddObject3DUpdate() override;
+	void AddObject2DUpdate() override;
+	void AddCommonUpdate() override;
+
+	void AddBeatEndUpdate() override;
+
+	//描画
+	void AddObject3DDraw() override;
+	void AddObject2DDraw() override;
+	void AddParticleDraw() override;
+	void AddUIDraw() override;
+
+	//後処理
+	void AddObjectFinalize() override;
+	void AddCommonFinalize() override;
+
+#ifdef _DEBUG
+	void AddDebugUpdate() override;
+	void AddDebugDraw() override;
+	void AddDebugFinalize() override;
+#endif // _DEBUG
+
 private:
-	//衝突マネージャー
-	CollisionManager* collisionManager = nullptr;
-
-	//3Dオブジェクト
-
-	//player
-	std::unique_ptr<Player> player;
-	//セーブ体力
-	int saveHP = 0;
-
-	//Plane
-	static const int DIV_NUM = 11;
-	static const float Plane_Size;
-	std::unique_ptr<Planes> plane[DIV_NUM][DIV_NUM];
-	bool IsComboColorChange = false;
-
-	//skydome
-	std::unique_ptr<SampleObjObject> skydome;
-
-	//リズムカウント
-	RhythmManager* rhythmManager = nullptr;
-
-	//ゲームマネージャー
-	GameManager* gameManager = nullptr;
-
-	//リズム入力
-	bool IsRhythmInput = false;
-
-	//岩
-	std::unique_ptr<AreaOutRock> rock;
 
 	//コイン
 	std::unique_ptr<Coins> coin;
-	//出口
-	std::unique_ptr<Exit> exit;
+
+	std::unique_ptr<TrainingDummy> dummy;
 
 	//スプライト
 	std::unique_ptr<BaseSprites> buttonSp[4];
 	bool IsButtonPush[4] = {};
-
-#ifdef _DEBUG
-	//カメラ移動、回転変更フラグ
-	bool IsCameraMovementChange = true;
-#endif // _DEBUG
-
-	std::unique_ptr<TrainingDummy> dummy;
-
-	//シーン遷移
-	std::unique_ptr<BaseSprites> fade;
-	DirectX::XMFLOAT4 fadeColor = {0,0,0,1};
-	Vector2 fadeInSize = {};
-	float fadeCurrentFrame = 0;
-	//Prev
-	bool IsPrevSceneChange = true;
-	//Next
-	bool IsNextSceneChange = false;
-
-	//GameOver
-	bool IsGameEnd = false;
 };
