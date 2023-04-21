3aa4539ac23d0ee37af4b0c9bc88563558cd5039
k020g1146 <k020g1146@g.neec.ac.jp>
2023-04-21 09:46:14 +0900

    リリース用設定

--- Project/Engine/Base/App/Application.cpp
+++ Project/Engine/Base/App/Application.cpp
@@ -143,7 +143,7 @@ void Application::Initialize()
 #endif // _DEBUG
 
 	sceneManager = SceneManager::GetInstance();
-	BaseScene* scene = new HomeScene(dxCommon, window);
+	BaseScene* scene = new TitleScene(dxCommon, window);
 
 #ifdef _DEBUG
 	scene->SetDebugText(debugText);
