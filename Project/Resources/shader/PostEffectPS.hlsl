980ea9b1213dcee5ed4c6819b6a75355d2ab250b
k020g1146 <k020g1146@g.neec.ac.jp>
2023-06-08 21:34:13 +0900

    RadialBlur(放射状ブラー)を咆哮のポストエフェクトとして実装

--- Project/Resources/shader/PostEffectPS.hlsl
+++ Project/Resources/shader/PostEffectPS.hlsl
@@ -13,40 +13,17 @@ float4 main(VSOutput input) : SV_TARGET
 	}
 	//ぼかし
 	else if(isBlurActive){
-		//ガウシアンブラー
-		float totalWeight = 0;
-		float _Sigma = 0.01;
-		float _StepWidth = 0.001;
-		float Blur = _Sigma*2;
-		float4 col = float4(0,0,0,1);
-
-        for(float py = -Blur; py <= +Blur; py +=_StepWidth){
-            for(float px = -Blur; px <= +Blur; px +=_StepWidth){
-
-                float2 pickUV = input.uv + float2(px,py);
-                float weight = Gaussian(input.uv, pickUV, _Sigma);
-                col += tex.Sample(smp, pickUV) * weight;
-                totalWeight += weight;
-            }
-        }
-        col.rgb = col.rgb / totalWeight;
-
-
-		//円内判定
-		float2 center = float2(0.5f,0.45f);
-		float radius1 = 0.15;
-		float radius2 = 1.0f;
-		float2 calValue = float2((input.uv.x-center.x)*(input.uv.x-center.x), (input.uv.y-center.y)*(input.uv.y-center.y));
-		float radius1x2 = radius1*radius1;
-		float radius2x2 = radius2*radius2;
-
-		//条件により戻り
-		if(calValue.x+calValue.y <= radius1x2){
-			return float4(texcolor.rgb,1);
+		float4 col = {0,0,0,0};
+		float2 symmetryUV = input.uv - 0.5f;
+		float distance = length(symmetryUV);
+		float factor = 1.0f / 16 * distance;
+		for(int j = 0; j < 16; j++){
+			float uvOffset = 1 - factor * j;
+			col += tex.Sample(smp, symmetryUV * uvOffset + 0.5);
 		}
-		else if(calValue.x+calValue.y <= radius2x2){
-			return float4(col.rgb, 1);
-		}
+
+		col /= 16;
+		return col;
 	}
 
 	return float4(texcolor.rgb,1);
