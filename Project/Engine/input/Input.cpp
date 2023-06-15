2e49d9b11130cd15e0b4827068fb5cc757c3f302
k020g1146 <k020g1146@g.neec.ac.jp>
2023-06-15 13:05:24 +0900

    エンジンの追加、マウス入力可能

--- Project/Engine/input/Input.cpp
+++ Project/Engine/input/Input.cpp
@@ -1,4 +1,4 @@
-﻿#include "Input.h"
+#include "Input.h"
 #include <cassert>
 
 #pragma comment(lib, "dinput8.lib")
@@ -17,9 +17,10 @@ void Input::Initialize(HWND hwnd)
 {
 	HRESULT result;
 
+	hwnd_ = hwnd;
+
 	///DirectInPut
 	//初期化 (他入力方法追加でもこのオブジェクトは一つのみ)
-	IDirectInput8* directInput = nullptr;
 	HINSTANCE hInstance = GetModuleHandle(nullptr);
 	result = DirectInput8Create(
 		hInstance, 
@@ -30,25 +31,44 @@ void Input::Initialize(HWND hwnd)
 	);
 	assert(SUCCEEDED(result));
 
+
 	//キーボードデバイスの生成 (GUID_Joystick (ジョイステック)、 GUID_SysMouse (マウス))
 	result = directInput->CreateDevice(
 		GUID_SysKeyboard,
-		&keyboard,
+		&keyboard_,
+		NULL
+	);
+	assert(SUCCEEDED(result));
+	//マウス
+	result = directInput->CreateDevice(
+		GUID_SysMouse,
+		&mouse_,
 		NULL
 	);
 	assert(SUCCEEDED(result));
 
+
 	//入力データ形式のセット (入力デバイスの種類によって、あらかじめ何種類か用意する)
-	result = keyboard->SetDataFormat(&c_dfDIKeyboard);	//標準形式
+	result = keyboard_->SetDataFormat(&c_dfDIKeyboard);	//標準形式
 	assert(SUCCEEDED(result));
+	//マウス
+	result = mouse_->SetDataFormat(&c_dfDIMouse);
+	assert(SUCCEEDED(result));
+
 
 	//排他的制御レベルのセット
 	//DISCL_FOREGROUND		画面が手前にある場合のみ入力を受け付ける
 	//DISCL_NONEXCLUSIVE	デバイスをこのアプリだけで専有しない
 	//DISCL_NOWINKEY		Windowsキーを無効にする
-	result = keyboard->SetCooperativeLevel(
-		hwnd,
+	result = keyboard_->SetCooperativeLevel(
+		hwnd_,
 		DISCL_FOREGROUND | DISCL_NONEXCLUSIVE | DISCL_NOWINKEY
+	);
+	assert(SUCCEEDED(result));
+	//マウス
+	result = mouse_->SetCooperativeLevel(
+		hwnd_,
+		DISCL_NONEXCLUSIVE | DISCL_FOREGROUND
 	);
 	assert(SUCCEEDED(result));
 }
@@ -58,25 +78,31 @@ void Input::Update()
 	HRESULT result;
 
 	//動作開始
-	result = keyboard->Acquire();
+	result = keyboard_->Acquire();
+	result = mouse_->Acquire();
+	result = mouse_->Poll();
 	//assert(SUCCEEDED(result));
 
 	//前回のキー入力情報コピー
 	for(int i = 0; i <KeyNum; i++){
-		prekey[i] = key[i];
+		prekey_[i] = key_[i];
 	}
+	mousePreKey_ = mouseKey_;
 
-	//キー入力
-	result = keyboard->GetDeviceState(sizeof(key), key); 
+
+	//状態取得
+	result = keyboard_->GetDeviceState(sizeof(key_), key_); 
 	//assert(SUCCEEDED(result));
+	result = mouse_->GetDeviceState(sizeof(mouseKey_), &mouseKey_);
 }
+
 
 bool Input::Push(int keyNumber)
 {
 	if(keyNumber < 0x00) return false;
 	if(keyNumber > 0xff) return false;
 
-	if(key[keyNumber])
+	if(key_[keyNumber])
 	{
 		return true;
 	}
@@ -88,9 +114,40 @@ bool Input::Trigger(int keyNumber)
 	if(keyNumber < 0x00) return false;
 	if(keyNumber > 0xff) return false;
 
-	if(key[keyNumber] && !prekey[keyNumber])
+	if(key_[keyNumber] && !prekey_[keyNumber])
 	{
 		return true;
 	}
 	return false;
+}
+
+
+bool Input::MousePush(int keyNumber)
+{
+	if(mouseKey_.rgbButtons[keyNumber] & (MOUSE_ON_VALUE)){
+		return true;
+	}
+	return false;
+}
+
+bool Input::MouseTrigger(int keyNumber)
+{
+	if((mouseKey_.rgbButtons[keyNumber] & (MOUSE_ON_VALUE)) && !(mousePreKey_.rgbButtons[keyNumber] & (MOUSE_ON_VALUE))){
+		return true;
+	}
+	return false;
+}
+
+const Vector2 Input::GetMousePos()
+{
+	POINT p;
+	GetCursorPos(&p);
+	ScreenToClient(hwnd_, &p);
+
+	return Vector2{(float)p.x,(float)p.y};
+}
+
+const Vector2 Input::GetMouseVelocity()
+{
+	return Vector2((float)mouseKey_.lX, (float)mouseKey_.lY);
 }
--- Project/Engine/input/Input.h
+++ Project/Engine/input/Input.h
@@ -1,9 +1,13 @@
 ﻿#pragma once
+#include "Vector2.h"
 
 //キーボード入力
 #define DIRECTINPUT_VERSION 0x0800	//DirectInputのバージョン指定
 #include <dinput.h>
 const int KeyNum = 256;
+
+//マウス
+#define MOUSE_ON_VALUE 0x80
 
 class Input
 {
@@ -24,14 +28,43 @@ public:
 	bool Trigger(int keyNumber);
 #pragma endregion
 
+#pragma region マウス入力
+	/// <summary>
+	/// 0:Left, 1;Right, 2:Center
+	/// </summary>
+	bool MousePush(int keyNumber);
+	bool MouseTrigger(int keyNumber);
+	
+	//マウスの座標
+	const Vector2 GetMousePos();
+	//マウスの移動量
+	const Vector2 GetMouseVelocity();
+
+#pragma endregion
+
 private:
+	HWND hwnd_;
+	IDirectInput8* directInput = nullptr;
+
 #pragma region キーボード
 	//キーボードデバイス
-	IDirectInputDevice8* keyboard = nullptr;
+	IDirectInputDevice8* keyboard_ = nullptr;
 	//キー情報
-	BYTE key[KeyNum] = {};
+	BYTE key_[KeyNum] = {};
 	//前フレームキー情報
-	BYTE prekey[KeyNum] = {};
+	BYTE prekey_[KeyNum] = {};
+#pragma endregion
+
+
+#pragma region マウス
+	//マウスデバイス
+	IDirectInputDevice8* mouse_ = nullptr;
+	//マウス情報
+	DIMOUSESTATE mouseKey_;
+	DIMOUSESTATE mousePreKey_;
+
+	//座標
+	Vector2 mousePos_;
 #pragma endregion
 };
 
